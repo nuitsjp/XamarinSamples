e664bef334032a5c2b1e66c29587901742e11657
nuits.jp <nuits.jp@live.jp>
2016-06-27 18:40:47 +0900

    XFormsBindingSample追加

---
+++ XFormsBindingSample/XFormsBindingSample/XFormsBindingSample/ElementNameSubstitutePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XFormsBindingSample
+{
+    public partial class ElementNameSubstitutePage : ContentPage
+    {
+        public ElementNameSubstitutePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
