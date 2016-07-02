f5ddf75d952f6f7b07be0df724460a05ee20a203
nuits.jp <nuits.jp@live.jp>
2016-07-02 17:03:57 +0900

    XFormsMemoryLeakTester追加

---
+++ XFormsMemoryLeakTester/XFormsMemoryLeakTester/XFormsMemoryLeakTester/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Reactive.Linq;
+
+namespace XFormsMemoryLeakTester
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
+
+        public ICommand StartCommand { get; }
+
+        public ICommand StopCommand { get; }
+        public ICommand GcCommand { get; } = new RelayCommand(() => GC.Collect());
+
+        private IDisposable TimerSubscription { get; set; }
+
+        const int MaxItemsCount = 10;
+
+        int counter = 0;
+
+        public MainPageViewModel()
+        {
+            StartCommand = new RelayCommand(() =>
+            {
+                if(TimerSubscription == null)
+                {
+                    TimerSubscription = Observable.Interval(TimeSpan.FromMilliseconds(20))
+                        .Subscribe(_ =>
+                        {
+                            Device.BeginInvokeOnMainThread(() =>
+                            {
+                                Items.Add(new Item { Label = $"Label{++counter}" });
+                                if (MaxItemsCount < Items.Count)
+                                    Items.Remove(Items[0]);
+                            });
+                        });
+                }
+            });
+            StopCommand = new RelayCommand(() =>
+            {
+                if (TimerSubscription != null)
+                {
+                    TimerSubscription.Dispose();
+                    TimerSubscription = null;
+                }
+            });
+        }
+    }
+}
---
+++ XFormsMemoryLeakTester/XFormsMemoryLeakTester/XFormsMemoryLeakTester/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XFormsMemoryLeakTester
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action execute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
