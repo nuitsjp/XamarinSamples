512663c106d502fd4a12eda67d531a78391fb10c
nuits.jp <nuits.jp@live.jp>
2016-07-21 11:43:40 +0900

    ServiceLocatorの実装・単体テスト

---
+++ XFStopwatch/XFStopwatch/XFStopwatch.Tests/ServiceLocatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XFStopwatch.Tests
+{
+    [TestClass]
+    public class ServiceLocatorTests
+    {
+        [TestMethod]
+        [Description("インスタンス指定で設定されたケースに正しく取得されることを確認する")]
+        public void ResolveNormalTest()
+        {
+            var service = new ServiceA();
+            ServiceLocator.Register<IServiceA>(service);
+
+            var resolved = ServiceLocator.Resolve<IServiceA>();
+            Assert.IsNotNull(resolved);
+            Assert.AreEqual(service, resolved);
+        }
+
+        [TestMethod]
+        public void ResolverNullTest()
+        {
+            ServiceLocator.Register<string>(null);
+            Assert.IsNull(ServiceLocator.Resolve<string>());
+        }
+
+        public interface IServiceA
+        {
+        }
+
+        public class ServiceA : IServiceA
+        {
+        }
+    }
+}
---
+++ XFStopwatch/XFStopwatch/XFStopwatch/ServiceLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFStopwatch
+{
+    public static class ServiceLocator
+    {
+        private static readonly Dictionary<Type, object> Resolvers = new Dictionary<Type, object>();
+
+        public static T Resolve<T>()
+        {
+            return (T)Resolvers[typeof(T)];
+        }
+
+        public static void Register<T>(T value)
+        {
+            Resolvers[typeof(T)] = value;
+        }
+    }
+}
