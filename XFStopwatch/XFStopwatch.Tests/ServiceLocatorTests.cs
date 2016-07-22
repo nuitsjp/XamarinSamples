33eb7f8ce6df34c14ac196b2161e905c3705c196
nuits.jp <nuits.jp@live.jp>
2016-07-22 09:03:55 +0900

    ServiceLocatorの実装

---
+++ XFStopwatch/XFStopwatch.Tests/ServiceLocatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XFStopwatch.Tests
+{
+    [TestClass]
+    public class ServiceLocatorTests
+    {
+        [TestMethod]
+        public void TestLocateNormal()
+        {
+            var sercice = new Service();
+            ServiceLocator.Register<IService>(sercice);
+
+            var actual = ServiceLocator.Locate<IService>();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(sercice, actual);
+        }
+
+        [TestMethod]
+        public void TestLocateNullValue()
+        {
+            ServiceLocator.Register<IService>(null);
+            Assert.IsNull(ServiceLocator.Locate<IService>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestLocateNotRegistered()
+        {
+            ServiceLocator.Locate<string>();
+        }
+
+        public interface IService
+        {
+        }
+
+        public class Service : IService
+        {
+        }
+    }
+}
---
+++ XFStopwatch/XFStopwatch/XFStopwatch/ServiceLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFStopwatch
+{
+    public static class ServiceLocator
+    {
+        private static readonly Dictionary<Type, object> Store = new Dictionary<Type, object>();
+        public static void Register<T>(T value)
+        {
+            Store[typeof(T)] = value;
+        }
+
+        public static T Locate<T>()
+        {
+            object result;
+            if (Store.TryGetValue(typeof(T), out result))
+            {
+                return (T)result;
+            }
+            else
+            {
+                throw new ArgumentException($"{typeof(T).FullName} is not registration.");
+            }
+        }
+    }
+}
