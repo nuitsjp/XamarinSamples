a3af6fffe8b7d6ca21bb1de9eebb23f8a89fb0ff
nuits.jp <nuits.jp@live.jp>
2016-07-24 23:33:46 +0900

    MainPageViewModelの一部の実装と単体テスト

--- XFStopwatch/XFStopwatch.ViewModels.Tests/MainPageViewModelTests.cs
+++ XFStopwatch/XFStopwatch.ViewModels.Tests/MainPageViewModelTests.cs
@@ -1,6 +1,46 @@
-﻿namespace XFStopwatch.ViewModels.Tests
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using XFStopwatch.Models;
+
+namespace XFStopwatch.ViewModels.Tests
 {
+    [TestClass]
     public class MainPageViewModelTests
     {
+        [TestMethod]
+        public void MainPageViewModelTest()
+        {
+            var stopwatch = new Mock<IStopwatch>();
+
+            var elapsedTime = TimeSpan.FromSeconds(100);
+            stopwatch.Setup(m => m.ElapsedTime).Returns(elapsedTime);
+
+            var status = StopwatchStatus.Paused;
+            stopwatch.Setup(m => m.Status).Returns(status);
+
+            var lapTime = new LapTime(1, TimeSpan.MaxValue);
+            var lapTimes = new ReadOnlyObservableCollection<LapTime>(new ObservableCollection<LapTime> { lapTime });
+            stopwatch.Setup(m => m.LapTimes).Returns(lapTimes);
+
+            var viewModel = new MainPageViewModel(stopwatch.Object);
+
+
+            Assert.AreEqual(elapsedTime, viewModel.ElapsedTime);
+            Assert.AreEqual(status, viewModel.Status);
+            Assert.IsNotNull(viewModel.LapTimes);
+            Assert.AreEqual(1, viewModel.LapTimes.Count);
+            Assert.AreEqual(lapTime, viewModel.LapTimes[0]);
+
+            Assert.IsNotNull(viewModel.StartOrStopCommand);
+            Assert.IsTrue(viewModel.StartOrStopCommand.CanExecute(null));
+            viewModel.StartOrStopCommand.Execute(null);
+            stopwatch.Verify(m => m.Start());
+
+            Assert.IsNotNull(viewModel.PauseOrResetCommand);
+            Assert.IsFalse(viewModel.PauseOrResetCommand.CanExecute(null));
+
+        }
     }
 }
--- XFStopwatch/XFStopwatch.ViewModels/MainPageViewModel.cs
+++ XFStopwatch/XFStopwatch.ViewModels/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using Xamarin.Forms;
 using XFStopwatch.Models;
 
 namespace XFStopwatch.ViewModels
@@ -12,20 +13,36 @@ namespace XFStopwatch.ViewModels
     {
         private readonly IStopwatch _stopwatch;
         public TimeSpan ElapsedTime { get; }
-
-        public ReadOnlyObservableCollection<LapTime> LapTimes { get; }
-
         public StopwatchStatus Status { get; }
-
+        public ReadOnlyObservableCollection<LapTime> LapTimes { get; }
         public ICommand StartOrStopCommand { get; }
-
         public ICommand PauseOrResetCommand { get; }
-
         public ICommand NavigationDetailCommand { get; }
 
         public MainPageViewModel(IStopwatch stopwatch)
         {
             _stopwatch = stopwatch;
+            ElapsedTime = _stopwatch.ElapsedTime;
+            Status = _stopwatch.Status;
+            LapTimes = new ReadOnlyObservableCollection<LapTime>(new ObservableCollection<LapTime>(_stopwatch.LapTimes));
+
+            StartOrStopCommand = new Command(OnStartOrStopCommand);
+            PauseOrResetCommand = new Command(OnPauseOrResetCommand, CanPauseOrResetCommandExecute);
+        }
+
+        private void OnStartOrStopCommand()
+        {
+            _stopwatch.Start();
+        }
+
+        private void OnPauseOrResetCommand()
+        {
+            
+        }
+
+        private bool CanPauseOrResetCommandExecute()
+        {
+            return true;
         }
     }
 }
