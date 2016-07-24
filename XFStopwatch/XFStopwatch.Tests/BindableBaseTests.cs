e9535c5cd6310744389e6e8bbec15cb98fd09ea9
nuits.jp <nuits.jp@live.jp>
2016-07-24 13:54:14 +0900

    BindableBaseの追加

---
+++ XFStopwatch/XFStopwatch.Tests/BindableBaseTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XFStopwatch.Tests
+{
+    [TestClass]
+    public class BindableBaseTests
+    {
+        [TestMethod]
+        public void IsNotifiedTest()
+        {
+            var bindable = new TestBindable();
+            bool isNotified = false;
+            bindable.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Property")
+                {
+                    isNotified = true;
+                }
+            };
+
+            bindable.Property = "Hello, World.";
+
+            Assert.IsTrue(isNotified);
+            Assert.AreEqual("Hello, World.", bindable.property);
+        }
+
+        [TestMethod]
+        public void IsNotNotifiedTest()
+        {
+            var bindable = new TestBindable();
+            bool isNotified = false;
+            bindable.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Property")
+                {
+                    isNotified = true;
+                }
+            };
+
+            bindable.Property = null;
+
+            Assert.IsFalse(isNotified);
+            Assert.IsNull(bindable.property);
+        }
+
+        public class TestBindable : BindableBase
+        {
+            // ReSharper disable once InconsistentNaming
+            public string property;
+
+            public string Property
+            {
+                get { return property; }
+                set { result = SetProperty(ref property, value); }
+            }
+
+            // ReSharper disable once InconsistentNaming
+            public bool result;
+        }
+    }
+}
--- XFStopwatch/XFStopwatch/XFStopwatch/BindableBase.cs
+++ XFStopwatch/XFStopwatch/XFStopwatch/BindableBase.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using XFStopwatch.Annotations;
 
-namespace XFStopwatch.ViewModels
+namespace XFStopwatch
 {
-    public class MainPageViewModel : INotifyPropertyChanged
+    public abstract class BindableBase : INotifyPropertyChanged
     {
-
-
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-        private bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        protected bool SetProperty<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(property, value))
             {
