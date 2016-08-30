82cdc44f9b0b414cc915d55001d8b458b5fd2aeb
nuits.jp <nuits.jp@live.jp>
2016-08-30 18:39:56 +0900

    ReflectionIOSSample

---
+++ ReflectionIOSSample/ReflectionIOSSample/ViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reactive.Linq;
+using UIKit;
+
+namespace ReflectionIOSSample
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            Observable.FromEventPattern(MyButton, "TouchUpInside")
+                .Subscribe(x => MyButton.SetTitle("Clicked!", UIControlState.Normal));
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
