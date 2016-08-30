f6a14207cb90c22ec6bcf04692bc05af6b2aeea8
nuits.jp <nuits.jp@live.jp>
2016-08-30 19:10:34 +0900

    RxではなくReflectionを通常に利用する方法に修正してみる

--- ReflectionIOSSample/ReflectionIOSSample/ViewController.cs
+++ ReflectionIOSSample/ReflectionIOSSample/ViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reactive.Linq;
+using System.Reflection;
 using UIKit;
 
 namespace ReflectionIOSSample
@@ -14,8 +15,23 @@ namespace ReflectionIOSSample
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
-            Observable.FromEventPattern(MyButton, "TouchUpInside")
-                .Subscribe(x => MyButton.SetTitle("Clicked!", UIControlState.Normal));
+            //Observable.FromEventPattern(MyButton, "TouchUpInside")
+            //    .Subscribe(x => MyButton.SetTitle("Clicked!", UIControlState.Normal));
+
+
+            var eventInfo = MyButton.GetType().GetRuntimeEvent("TouchUpInside");
+            if (eventInfo == null)
+                throw new ArgumentException($"EventToCommandBehavior: Can't register the 'TouchUpInside' event.");
+
+            // OnEventメソッドでイベントを購読するため、MethodInfoからデリゲートを作成しイベントへ追加する
+            MethodInfo methodInfo = typeof(ViewController).GetTypeInfo().GetDeclaredMethod("OnEvent");
+            var eventHandler = methodInfo.CreateDelegate(eventInfo.EventHandlerType, this);
+            eventInfo.AddEventHandler(MyButton, eventHandler);
+        }
+
+        private void OnEvent(object sender, EventArgs e)
+        {
+            MyButton.SetTitle("Clicked!", UIControlState.Normal);
         }
 
         public override void DidReceiveMemoryWarning()
