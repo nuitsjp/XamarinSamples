8cb76bedc0fe4d4b9709fe4788436ee670ca0772
nuits.jp <nuits.jp@live.jp>
2016-09-19 23:07:10 +0900

    MasterDetailSampleの追加

---
+++ MasterDetailSample/MasterDetailSample/MasterDetailSample/Views/ContactsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MasterDetailSample.Views
+{
+    public partial class ContactsPage : ContentPage
+    {
+        public ContactsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MasterDetailSample/MasterDetailSample/MasterDetailSample/Views/MasterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MasterDetailSample.Views
+{
+    public partial class MasterPage : ContentPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
