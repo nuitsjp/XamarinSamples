77a25b058cf14441ab348f2343abda7d22af0596
nuits.jp <nuits.jp@live.jp>
2016-12-27 09:41:49 +0900

    Add XAndroidHideStatusBar.

---
+++ XAndroidHideStatusBar/XAndroidHideStatusBar/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace XAndroidHideStatusBar
+{
+    [Activity(Label = "XAndroidHideStatusBar", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            Window.AddFlags(WindowManagerFlags.Fullscreen);
+            Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
+
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
