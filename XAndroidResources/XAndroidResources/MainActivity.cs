5353ed66fe66ca9d5350accf45ad27df49b0f8f5
nuits.jp <nuits.jp@live.jp>
2016-12-29 00:44:47 +0900

    Add XAndroidResource.

---
+++ XAndroidResources/XAndroidResources/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using System.Xml;
+
+namespace XAndroidResources
+{
+    [Activity(Label = "XAndroidResources", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            string message;
+            using (var stream = new StreamReader(Assets.Open("MyXml.xml")))
+            {
+                XmlDocument document = new XmlDocument();
+                document.Load(stream);
+                message = document.ChildNodes[1].InnerText;
+            }
+
+            SetContentView (Resource.Layout.Main);
+
+            var messageTextView = FindViewById<TextView>(Resource.Id.MessageTextView);
+            messageTextView.Text = message;
+        }
+    }
+}
+
