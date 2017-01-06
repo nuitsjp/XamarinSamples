7c95500e1103765f33d45cbf1d99a3c7ee542168
nuits.jp <nuits.jp@live.jp>
2017-01-06 22:53:05 +0900

    Add PCLStorageSample.

---
+++ PCLStorageSample/PCLStorageSample/PCLStorageSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using PCLStorage;
+
+namespace PCLStorageSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public DelegateCommand SaveCommand => new DelegateCommand(Save);
+
+        public DelegateCommand LoadCommand => new DelegateCommand(Load);
+
+        private async void Load()
+        {
+            var file = await GetTextFile();
+
+            using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
+            using (var reader = new StreamReader(stream))
+            {
+                Title = reader.ReadToEnd();
+            }
+        }
+
+        private async void Save()
+        {
+            var file = await GetTextFile();
+            
+            using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(Title);
+                writer.Flush();
+            }
+        }
+
+
+
+        private static async Task<IFile> GetTextFile()
+        {
+            var folder = FileSystem.Current.LocalStorage;
+            return await folder.CreateFileAsync("text.txt", CreationCollisionOption.OpenIfExists);
+        }
+
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
