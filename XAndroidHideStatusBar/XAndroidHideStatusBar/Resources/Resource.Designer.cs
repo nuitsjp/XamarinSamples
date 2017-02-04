3b4185f4b2d0ee0e616d3cdb6176b5dfbd408f88
nuits.jp <nuits.jp@live.jp>
2017-02-04 20:50:51 +0900

    Delete XFNavigationSample

--- XAndroidHideStatusBar/XAndroidHideStatusBar/Resources/Resource.Designer.cs
+++ XAndroidHideStatusBar/XAndroidHideStatusBar/Resources/Resource.Designer.cs
@@ -77,10 +77,7 @@ namespace XAndroidHideStatusBar
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int FragmentMain = 2130903040;
-			
-			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
