45223c9100313b7f832d646c242fbf113d44d7f8
Atsushi Nakamura <nuits.jp@live.jp>
2017-08-12 10:04:56 +0900

    Add RxPropForXamarin

---
+++ RxPropForXamarin/RxPropForXamarin/RxPropForXamarin/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+
+namespace RxPropForXamarin
+{
+    public class MainPageViewModel
+    {
+        private readonly ReactiveProperty<int> _counter = new ReactiveProperty<int>(0);
+        public ReactiveProperty<string> Message { get; }
+
+        public ReactiveCommand UpdateMessageCommand { get; }
+
+        public MainPageViewModel()
+        {
+            Message = _counter.Select(x => $"Number of update executions:{x}").ToReactiveProperty();
+
+            UpdateMessageCommand = new ReactiveCommand();
+            UpdateMessageCommand.Subscribe(_ => { _counter.Value++; });
+        }
+    }
+}
