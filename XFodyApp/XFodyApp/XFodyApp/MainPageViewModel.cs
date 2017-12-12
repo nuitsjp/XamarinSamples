3afcea2781e4d944ab12a8582fcaa6a25779fb83
Atsushi Nakamura <nuits.jp@live.jp>
2017-12-12 15:44:15 +0900

    Add XFodyApp

---
+++ XFodyApp/XFodyApp/XFodyApp/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFodyApp
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set => SetProperty(ref _count, value);
+        }
+
+        public ICommand CountUpCommand => new Command(CountUp);
+
+        private void CountUp()
+        {
+            Count++;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool SetProperty<T>(ref T field, T value, [CallerMemberName]string callerMemberName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerMemberName));
+            return true;
+        }
+        #endregion
+    }
+}
